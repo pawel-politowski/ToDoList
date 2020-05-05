402267d3e0df17f0d9809233e5c1d9e292ff6640
Paweł Politowski <matelly@poczta.onet.pl>
2020-05-05 22:27:29 +0200

    Updates

---
+++ precache-manifest.5df597d79bb98117ed442d60d15730b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e549e395d15be8447ab40123f8228af5",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "a01c708389f9c1c6ed7c",
+    "url": "/ToDoList/static/css/main.54b0d2f2.chunk.css"
+  },
+  {
+    "revision": "b3f4b5dbf61b7ebd7545",
+    "url": "/ToDoList/static/js/2.86e06524.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoList/static/js/2.86e06524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01c708389f9c1c6ed7c",
+    "url": "/ToDoList/static/js/main.3aa332a6.chunk.js"
+  },
+  {
+    "revision": "894e80690669648fd0df",
+    "url": "/ToDoList/static/js/runtime-main.c148c603.js"
+  }
+]);
